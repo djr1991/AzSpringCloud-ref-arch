76946c164f91596a840c07dd11758eae7d6f6ac1
Dylan Reed <dyreed@microsoft.com>
2020-12-14 11:02:04 -0500

    add fw rules

--- deploy-asc-internal.sh
+++ deploy-asc-internal.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #parameters
+randomstring=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 15 | head -n 1)
 location='eastus' #location of Azure Spring Cloud Virtual Network
 hub_vnet_name='hub-vnet' #Hub Virtual Network Name
-hub_vnet_resource_group_name='hub-vnet-rg2' #Hub Virtual Network Resource Group name
+hub_vnet_resource_group_name='hub-vnet-rg' #Hub Virtual Network Resource Group name
 hub_vnet_address_prefixes='10.9.0.0/16' #Hub Virtual Network Address Prefixes
 firewal_subnet_prefix='10.9.0.0/24' #Address prefix of 
 centralized_services_subnet_name='centralized-services-subnet'
@@ -14,6 +15,7 @@ application_gateway_subnet_name='application-gateway-subnet'
 application_gateway_subnet_prefix='10.9.3.0/24'
 firewall_name='azfirewall' #Name of Azure firewall resource
 firewall_public_ip_name='azfirewall-pip2' #Azure firewall public ip resource name
+azure_key_vault_name='akv-'$randomstring #Azure Key vault unique name
 azurespringcloud_vnet_resource_group_name='azurespringcloud-spoke-vnet-rg' #parameter for Azure Spring Cloud Virtual network resource group name
 azurespringcloud_vnet_name='azurespringcloud-spoke-vnet' #parameter for Azure Spring Cloud Vnet name
 azurespringcloud_vnet_address_prefixes='10.8.0.0/16' #address prefix of Azure Spring Cloud Virtual Network
@@ -22,11 +24,15 @@ azurespringcloud_service_runtime_subnet_name='service-runtime-subnet'
 azurespringcloud_app_subnet_prefix='10.8.1.0/24'
 azurespringcloud_app_subnet_name='apps-subnet'
 azurespringcloud_resource_group_name='azspringcloud-rg' #Hub Virtual Network Resource Group name
-azurespringcloud_service='azspringcloud-dylanjreed-test' #Name of Spring Cloud resource
-azurespringcloud_service_runtime_resource_group_name='azspringcloud-service-runtime-rg-test' #Name of Azure Spring Cloud service runtime resource group	
-azurespringcloud_app_resource_group_name='azspringcloud-apps-rg-test' #Name of Azure Spring Cloud apps resource group
+azurespringcloud_service='azspringcloud-'$randomstring #Name of unique Spring Cloud resource
+azurespringcloud_service_runtime_resource_group_name=$azurespringcloud_service'-service-runtime-rg' #Name of Azure Spring Cloud service runtime resource group	
+azurespringcloud_app_resource_group_name=$azurespringcloud_service'-apps-rg' #Name of Azure Spring Cloud apps resource group
 
-echo create hub vnet rg
+echo "Enter full UPN of Key Vault Admin: "
+read userupn
+admin_object_id=$(az ad user show --id $userupn --query objectId --output tsv)
+
+echo "create hub vnet rg"
 
 az group create --location ${location} --name ${hub_vnet_resource_group_name}
 
@@ -71,7 +77,6 @@ az network vnet subnet create \
 echo all subnets and vnet created
 
 echo create FW
-
 az network firewall create \
     --name ${firewall_name} \
     --resource-group ${hub_vnet_resource_group_name} \
@@ -93,6 +98,20 @@ az network firewall update \
     --name ${firewall_name}  \
     --resource-group ${hub_vnet_resource_group_name}
 firewall_private_ip="$(az network firewall ip-config list -g ${hub_vnet_resource_group_name} -f ${firewall_name} --query "[?name=='FW-config'].privateIpAddress" --output tsv)"
+
+echo create FW network rules
+az network firewall network-rule create \
+    --collection-name SpringCloudAccess \
+    --destination-ports 123 \
+    --firewall-name ${firewall_name} \
+    --name NtpQuery \
+    --protocols UDP \
+    --resource-group ${hub_vnet_resource_group_name} \
+    --action Allow \
+    --destination-addresses * \
+    --source-addresses ${azurespringcloud_app_subnet_prefix} ${azurespringcloud_service_runtime_subnet_prefix}
+    --priority 100
+echo Finished creating FW rules
 
 echo create Azure spring cloud vnet rg
 
@@ -169,6 +188,12 @@ echo finished peering of hub to ASC spoke
 echo create Azure Spring Cloud Resource group
 
 az group create --location ${location} --name ${azurespringcloud_resource_group_name}
+
+echo creating spring cloud AKV
+az keyvault create --name ${azure_key_vault_name} --resource-group ${azurespringcloud_resource_group_name} --location ${location} --no-self-perms
+echo finished creating azure spring cloud akv
+
+az keyvault set-policy --name ${azure_key_vault_name} --object-id $admin_object_id  --key-permissions backup create decrypt delete encrypt get import list purge recover restore sign unwrapKey update verify wrapKey --secret-permissions backup delete get list purge recover restore set --certificate-permissions backup create delete deleteissuers get getissuers import list listissuers managecontacts manageissuers purge recover restore setissuers update
 
 
 echo Getting app subnet id
